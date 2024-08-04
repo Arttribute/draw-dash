1e727a9e5846d94f05cb8cf0ca12db8df1b0cf7c
Abigail Muthoni Wairi <abby.muso@gmail.com>
2024-08-04 14:33:21 +0300

    Feat: Created the game screen comonent

---
+++ app/play/page.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import GameScreen from '@/components/game/GameScreen'; // Adjust the path if necessary
+
+const Play = () => {
+  return (
+    <div>
+      <GameScreen />
+    </div>
+  );
+};
+
+export default Play;
