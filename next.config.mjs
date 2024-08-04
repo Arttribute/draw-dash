f8545912b1867d4698266ecee76fc30adc4575c6
khalifa47 <khalifafumo5@gmail.com>
2024-08-04 15:42:54 +0300

    feat: set up transformers.js for image comparison
    - add next.js config for transformers support.
    - add application singleton containing required tokenizers and vision models from transformers
    - create API route files where comparison logic will go

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  experimental: {
+    serverComponentsExternalPackages: ['sharp', 'onnxruntime-node'],
+  },
   images: {
     remotePatterns: [
       {
