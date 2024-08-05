b5c73ab0f490d8e5b90e95af5c9691c5b49b0615
khalifa47 <khalifafumo5@gmail.com>
2024-08-05 20:18:31 +0300

    feat: tokenize and encode input image
    For now, we create a dummy form component allowing image passing into the APi where transformers is used to process the image input (in URL format) and is encoded into 512 dim array.
    
    TODO: perform cosine similarity comparison

---
+++ app/api/compare/route.ts
@@ -0,0 +1,55 @@
+import ApplicationSingleton from "@/app/app";
+import { NextResponse } from "next/server";
+import {
+  type PreTrainedModel,
+  type Processor,
+  RawImage,
+} from "@xenova/transformers";
+
+type Input = {
+  query_image: string;
+  ans_image: string;
+};
+
+export async function POST(request: Request) {
+  try {
+    // const { query_image, ans_image }: Input = await request.json();
+
+    // TODO: upload to bucket and get the URL
+
+    const [processor, vision_model]: [Processor, PreTrainedModel] =
+      await ApplicationSingleton.getInstance();
+
+    // Read the two images
+    const rawImageQuery = await RawImage.read(
+      "https://img.freepik.com/free-vector/grunge-floral-background_1048-7366.jpg?size=626&ext=jpg&ga=GA1.1.407704436.1720632020&semt=ais_hybrid"
+    );
+    const rawImageAns = await RawImage.read(
+      "https://img.freepik.com/free-photo/yellow-throated-sericornis-sericornis-citreogularis-illustrated-by-elizabeth-gould_53876-65174.jpg?size=626&ext=jpg&ga=GA1.1.407704436.1720632020&semt=sph"
+    );
+
+    // Tokenize the two images
+    const tokenizedImageQuery = await processor(rawImageQuery);
+    const tokenizedImageAns = await processor(rawImageAns);
+
+    // Encode the two images
+    const { image_embeds: embedsQuery } = await vision_model(
+      tokenizedImageQuery
+    );
+    const { image_embeds: embedsAns } = await vision_model(tokenizedImageAns);
+
+    // TODO: Compare the two images
+
+    return new NextResponse(
+      JSON.stringify({ okay: "okay", embedsQuery, embedsAns }),
+      {
+        status: 200,
+      }
+    );
+  } catch (error) {
+    console.error(error);
+    return new NextResponse(JSON.stringify({ error }), {
+      status: 500,
+    });
+  }
+}
--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,34 @@
+"use client";
 import Image from "next/image";
 
 import AccountMenu from "@/components/account/AccountMenu";
+
+async function handleSubmit(e: React.FormEvent<HTMLFormElement>) {
+  e.preventDefault();
+  const formData = new FormData(e.currentTarget);
+
+  const file = formData.get("query_image") as File;
+
+  const res = await fetch("/api/compare", {
+    method: "POST",
+    body: JSON.stringify({
+      query_image: URL.createObjectURL(file),
+    }),
+  });
+
+  const data = await res.json();
+  console.log(data);
+}
 
 export default function Home() {
   return (
     <div>
       Hello world
       <AccountMenu />
+      <form onSubmit={handleSubmit}>
+        <input type="file" accept="image/*" name="query_image" />
+        <button type="submit">Submit</button>
+      </form>
     </div>
   );
 }
