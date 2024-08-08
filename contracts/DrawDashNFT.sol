b0001c0bb278e73536de17f3ca1e33f702131286
bashybaranaba <bashybaranaba@gmail.com>
2024-08-09 00:57:26 +0300

    feat: add nft contract

--- contracts/DrawDashNFT.sol
+++ contracts/DrawDashNFT.sol
@@ -32,3 +32,22 @@ contract Lock {
         owner.transfer(address(this).balance);
     }
 }
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
+
+contract Tarot is ERC721URIStorage {
+    uint256 private _tokenIdCounter = 0;
+
+    constructor() ERC721("Draw Dash NFT", "DDASH") {}
+
+    function mintCertificate(address recipient, string memory tokenUri) public returns (uint256) {
+        _tokenIdCounter += 1;
+        uint256 newTokenId = _tokenIdCounter;
+        _safeMint(recipient, newTokenId);
+        _setTokenURI(newTokenId, tokenUri);
+        return newTokenId;
+    }
+}
