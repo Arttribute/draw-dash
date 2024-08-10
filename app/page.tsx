9899475e3deffcaaf3e5520f4991ae76d0fe09c5
bashybaranaba <bashybaranaba@gmail.com>
2024-08-11 01:53:12 +0300

    feat: homepage colors

--- app/page.tsx
+++ app/page.tsx
@@ -9,6 +9,15 @@ import Link from "next/link";
 import Image from "next/image";
 import CreationCard from "@/components/nft/CreationCard";
 
+import {
+  Card,
+  CardHeader,
+  CardContent,
+  CardFooter,
+  CardTitle,
+  CardDescription,
+} from "@/components/ui/card";
+
 const Home = () => {
   const [creations, setCreations] = useState([]);
   const [loadinigCreations, setLoadingCreations] = useState(false);
@@ -37,54 +46,77 @@ const Home = () => {
   }, []);
 
   return (
-    <div>
+    <>
       <AppBar />
-      <main className="container mx-auto px-6 lg:px-12 max-w-7xl mt-24">
-        <section className="my-12 mx-6 border p-8 rounded-md">
-          <div className="flex flex-col md:flex-row items-center justify-between">
-            <div className="w-full md:w-1/2 flex justify-center mb-8 md:mb-0">
-              <div className="w-full md:w-3/4">
-                <ImagePlaceholder />
-              </div>
-            </div>
-            <div className="w-full md:w-1/2 md:ml-20 text-left">
-              <h1 className="text-4xl font-bold mb-4">
-                Sketch Your Way to Victory
+      <div className="container mx-auto px-2 lg:px-12 lg:max-w-7xl mt-20 lg:mt-24 ">
+        <div className="lg:my-12 lg:mx-6 border py-10 lg:p-12 rounded-2xl bg-gradient-to-tl from-amber-200 via-pink-500 to-indigo-600 text-white ">
+          <div className="flex flex-col md:flex-row">
+            <div className="w-full mx-8 md:w-1/2 md:mx-20 text-left">
+              <h1 className="text-3xl  lg:text-6xl font-black mb-4 ">
+                DrawDash
               </h1>
-              <p className="text-lg mb-8 text-justify">
-                Welcome to DrawDash, where creativity meets competition. Given a
-                prompt, you must sketch an image as close as possible to the
-                hidden generated image within a given time. Challenge your
-                drawing skills and see how close you can get!
+
+              <p className="text-xs lg:text-lg mb-4 lg:mb-8 font-medium tracking-tight">
+                Sketch Your Way to Victory. Given a prompt, you must sketch an
+                image as close as possible to the hidden generated image within
+                a given time. Play, mint NFTs and earn rewards.
               </p>
-              <div className="flex justify-center md:justify-start space-x-4">
+
+              <div className="flex space-x-4">
                 <Link href="/play">
-                  <Button className="px-12">Play</Button>
+                  <Button className="px-16">Start Playing </Button>
                 </Link>
-                <Link href="#">
-                  <Button className="px-12">Create</Button>
-                </Link>
+                <div className="hidden lg:flex">
+                  <Link href="/play">
+                    <Button variant="outline" className="px-16 text-black">
+                      Start Earning{" "}
+                    </Button>
+                  </Link>
+                </div>
               </div>
             </div>
           </div>
-        </section>
+        </div>
 
-        <section className="my-12 mx-6 ">
-          <div className="grid grid-cols-1 md:grid-cols-2 gap-10">
-            <CustomCard
-              title="Play and Earn"
-              description="Show off your drawing skills by competing against others. Earn points and rewards based on how close your sketch is to the generated image."
-              buttonLabel="Deposit"
-            />
-            <CustomCard
-              title="NFT Marketplace"
-              description="Turn your best sketches into NFTs and sell them on our marketplace. Discover and collect unique artworks from other players."
-              buttonLabel="Enter Marketplace"
-            />
+        <div className="my-4 lg:my-10 lg:mx-6">
+          <div className="grid grid-cols-1 md:grid-cols-2 lg:gap-10 gap-4 ">
+            <Card className="bg-gradient-to-br from-purple-600 to-blue-400 text-white p-4 rounded-2xl">
+              <CardHeader>
+                <CardTitle>Play and Earn</CardTitle>
+              </CardHeader>
+              <CardContent>
+                <p className="hidden sm:block ">
+                  Show off your drawing skills by competing against others. Earn
+                  points and rewards based on how close your sketch is to the
+                  generated image
+                </p>
+              </CardContent>
+              <CardFooter>
+                <Button variant={"outline"} className="text-black">
+                  Start Earning
+                </Button>
+              </CardFooter>
+            </Card>
+
+            <Card className="bg-gradient-to-br from-yellow-200 to-amber-400  p-4 rounded-2xl">
+              <CardHeader>
+                <CardTitle>NFT Marketplace</CardTitle>
+              </CardHeader>
+              <CardContent>
+                <p className="hidden sm:block ">
+                  Turn your best sketches into NFTs and sell them on our
+                  marketplace. Discover and collect unique artworks from other
+                  players.
+                </p>
+              </CardContent>
+              <CardFooter>
+                <Button className="">Enter Marketplace</Button>
+              </CardFooter>
+            </Card>
           </div>
-        </section>
+        </div>
 
-        <section>
+        <div className="lg:mx-8">
           <h2 className="text-2xl font-bold mb-4">
             Creations from the Community
           </h2>
@@ -94,9 +126,9 @@ const Home = () => {
                 <CreationCard creation={creation} account={account} />
               </div>
             ))}
-        </section>
-      </main>
-    </div>
+        </div>
+      </div>
+    </>
   );
 };
 
