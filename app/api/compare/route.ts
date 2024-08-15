69f53a4ef623dc1a4b9700a305cbd01b5e25cd81
khalifa47 <khalifafumo5@gmail.com>
2024-08-15 23:11:34 +0300

    fix (attempt): fix compare function on vercel

--- app/api/compare/route.ts
+++ app/api/compare/route.ts
@@ -5,28 +5,7 @@ import {
   type Processor,
   RawImage,
 } from "@xenova/transformers";
-
-const cosineSimilarity = (query_embeds: number[], ans_embeds: number[]) => {
-  if (query_embeds.length !== ans_embeds.length) {
-    throw new Error("Embeddings must be of the same length");
-  }
-
-  let dotProduct = 0;
-  let normQuery = 0;
-  let normAns = 0;
-
-  for (let i = 0; i < query_embeds.length; ++i) {
-    const queryValue = query_embeds[i];
-    const dbValue = ans_embeds[i];
-
-    dotProduct += queryValue * dbValue;
-    normQuery += queryValue * queryValue;
-    normAns += dbValue * dbValue;
-  }
-
-  const similarity = dotProduct / (Math.sqrt(normQuery) * Math.sqrt(normAns));
-  return similarity;
-};
+import { cosineSimilarity } from "./util";
 
 export async function POST(request: Request) {
   try {
